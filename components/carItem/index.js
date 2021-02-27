0d41c927d92ac58a12680dab74defc5da44a6e9e
its-jahangeer <60851115+its-jahangeer@users.noreply.github.com>
2021-02-27 19:06:28 +0530

    first commit

---
+++ components/carItem/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View,Text,ImageBackground} from 'react native';
+import { StyledButton } from "./styledButton";
+import {styles} from './styles';
+
+const carItem = (props)=>{
+    return (
+        <View style={styles.carContainer}>
+     
+        <ImageBackground source={require('./assets/images/car1.jpg')}
+         style={styles.image}
+         />
+         <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,420</Text>
+        </View>
+        <StyledButton/>
+        </View>
+    );
+};
+export default carItem;
---
+++ components/styledButton/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+     width:'100%',
+     padding:10,
+    },
+    button:{
+         backgroundColor:'white',
+         height:40,
+    },
+    text:{
+
+    }
+
+    });
